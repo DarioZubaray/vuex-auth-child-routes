781197a66abb1daded70a7a91f6ae0eaf46d6b98
Dario Zubaray <dzubaray@gmail.com>
2019-06-05 08:37:46 -0300

    agregando el modulo de almacenamiento authentication

---
+++ src/modules/authentication/index.js
@@ -0,0 +1,10 @@
+import state from './state'
+import * as mutation from './mutation'
+import * as actions from './actions'
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VuexPersistence from 'vuex-persist'
+import auth from '@/modules/authentication'
 
 Vue.use(Vuex)
 
@@ -15,8 +16,8 @@ export default new Vuex.Store({
   mutations: {
 
   },
-  actions: {
-
-  },
+  modules: [
+    auth
+  ],
   plugins: [vuexLocal.plugin]
 })
