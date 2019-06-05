616561dd50fa5150fc6ccc1a35f1e24b01cedcb3
Dario Zubaray <dzubaray@gmail.com>
2019-06-05 09:00:29 -0300

    completando la vista de usuario logeado y definiendolas en el app junto con el loading overlay

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-
-    </div>
-    <router-view/>
+    <loading :active.sync="loading"></loading>
+    <b-container>
+      <navigation-logged v-if="isLogged"></navigation-logged>
+      <navigation-guest v-else></navigation-guest>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
+<script>
+import {mapState} from 'vuex'
+import NavigationLogged from '@/components/navigations/logged'
+import NavigationGuest from '@/components/navigations/guest'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+export default {
+  components: [
+    NavigationLogged, NavigationGuest
+  ],
+  computed: {
+    ...mapState('auth', ['isLogged']),
+    ...mapState(['loading'])
+  }
+}
+</script>
