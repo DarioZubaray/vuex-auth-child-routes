1f7ba2a6983f8777bcde9a24daea98f1045735ac
dario zubaray <dzubaray@gmail.com>
2019-06-09 15:56:55 -0300

    agregando una imagen con overlay al login

--- src/components/authentication/login.vue
+++ src/components/authentication/login.vue
@@ -1,31 +1,42 @@
 <template>
-    <b-form @submit.prevent="$emit('login')">
-        <b-form-group label="email" description="Los datos son privados">
-            <b-form-input type="email" 
-                          autocomplete="off" 
-                          v-model="user.email" 
-                          v-validate="'required|email'"
-                          name="email"
-                          placeholder="Introduce el email admin@vue.com">
-            </b-form-input>
-            <b-form-invalid-feedback> {{ errors.first('email') }} </b-form-invalid-feedback>
-        </b-form-group>
+    <b-card
+        overlay
+        img-src="./assets/pexels-photo-cyty.jpeg"
+        img-alt="Card Image"
+        text-variant="white"
+        title="Inicio de sesión"
+        sub-title="Enter the matrix"
+    >
+        <b-card-text>
+            <b-form @submit.prevent="$emit('login')">
+                <b-form-group label="email" description="Los datos son privados">
+                    <b-form-input type="email" 
+                                autocomplete="off" 
+                                v-model="user.email" 
+                                v-validate="'required|email'"
+                                name="email"
+                                placeholder="Introduce el email admin@vue.com">
+                    </b-form-input>
+                    <b-form-invalid-feedback> {{ errors.first('email') }} </b-form-invalid-feedback>
+                </b-form-group>
 
-        <b-form-group label="password">
-            <b-form-input type="password" 
-                          autocomplete="off" 
-                          v-model="user.password" 
-                          v-validate="'required|min:6'"
-                          name="password"
-                          placeholder="Introduce la contraseña @Password1">
-            </b-form-input>
-            <b-form-invalid-feedback> {{ errors.first('password') }} </b-form-invalid-feedback>
-        </b-form-group>
+                <b-form-group label="password">
+                    <b-form-input type="password" 
+                                autocomplete="off" 
+                                v-model="user.password" 
+                                v-validate="'required|min:6'"
+                                name="password"
+                                placeholder="Introduce la contraseña @Password1">
+                    </b-form-input>
+                    <b-form-invalid-feedback> {{ errors.first('password') }} </b-form-invalid-feedback>
+                </b-form-group>
 
-        <b-button :disabled="errors.any() || !user.password" type="submit" variant="primary">
-            Iniciar Sesión
-        </b-button>
-    </b-form>
+                <b-button :disabled="errors.any() || !user.password" type="submit" variant="primary">
+                    Iniciar Sesión
+                </b-button>
+            </b-form>
+        </b-card-text>
+    </b-card>
 </template>
 
 <script>
